65bd6662eb9b5acc1291409d373d637cc55e9ad6
RodCode <rodolfoantoniorq@gmail.com>
2025-09-14 00:14:21 +0200

    fix_video4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,24 +8,25 @@ export default function Home() {
       title: "Portfolio",
       description: "Una selección de piezas audiovisuales de mi gusto personal, acompañada de algunas muestras de mi trabajo en otros formatos. Todo reunido en una cápsula de un minuto, construida a través de un montaje rítmico e improvisado.",
       videoUrl: "https://youtu.be/ycfs8_WwkIk?si=AUTnqX9ZcJNBlAqV",
-      category: "Music Video"
+      category: "Video Edition"
     },
     {
       title: "Shut up, do Somenthing",
       description: "Un videoensayo que, a partir de un guion literario escrito en pocos días y un guion técnico en parte improvisado, explora el dilema de la persona como espectadora o como creadora. Una pieza que plantea la pregunta esencial: ¿serás uno más detrás de la pantalla o decidirás crear algo por ti mismo?",
       videoUrl: "https://youtu.be/BpphuoJOJwg?si=2efbcQvBZF6PGNu7",
-      category: "Commercial"
+      category: "Video Edition"
     },
     {
       title: " Improv With the Timeline",
       description: "Una selección de clips aleatorios unidos por una misma idea: rendir homenaje a la capacidad creativa del ser humano en todas sus formas. Un ejercicio de montaje rítmico y estilizado, concluido en apenas unas horas.",
       videoUrl: "https://youtu.be/wUl_57PEP8E?si=uMivIqoRUUOpGzHh",
-      category: "Sports"
+      category: "Video Edition"
     },
     {
-      title: "Iberia Airlines",
-      description: "Corporate video",
-      videoUrl: "https://www.youtube.com/watch?v=your-iberia-video-id"
+      title: "Finde the Opportunity",
+      description: "Después de una tarde de postulaciones y de recibir solo negativas como respuesta, decidí crear una pequeña cápsula narrativa con la intención de compartir un sentimiento común a quienes atraviesan la constante lucha de la búsqueda de empleo digital a través de la red.",
+      videoUrl: "https://youtu.be/1BtfbbPWUtA?si=d3eQAOyNbmKRvdn5",
+      category: "Video Edition"
     },
   ];
 
