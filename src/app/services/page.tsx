d308350dc37e0c33f6681d45f4a967c6d045d313
RodCode <rodolfoantoniorq@gmail.com>
2025-10-03 23:16:42 +0200

    loading updates

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import ServicesList from '../../components/ServicesList';
 
@@ -7,12 +8,10 @@ export default function ServicesPage() {
     <Header />
     <div className="min-h-screen pt-20 bg-black mt-8">
       <div className="container mx-auto px-4 py-16">
-        <h1 className="text-4xl md:text-5xl font-bold text-white mb-12 text-center">
-          Our Services
-        </h1>
         <ServicesList />
       </div>
     </div>
+    <Footer/>
     </>
   );
 }
